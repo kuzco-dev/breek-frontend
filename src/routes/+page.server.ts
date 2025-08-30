16b78b60e05986423d88df73a8c8d1efb6a86415
Kuzco <kuzco.dev@proton.me>
2025-08-30 01:32:46 +0100

    up api

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,7 +2,7 @@ import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
   try {
-    const response = await fetch('http://localhost:3001/teams');
+    const response = await fetch('http://168.231.85.107:3001/teams');
     const teams = await response.json();
     
     return {
